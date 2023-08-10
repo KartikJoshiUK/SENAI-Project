7f2fb8bb5014f6bbc114a9e84635fa5b66fc0050
KartikJoshiUK <kartikjoshiuk@gmail.com>
2023-08-10 14:13:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// IMPORTS
+const express = require("express");
+
+// Constants
+const app = express();
+const PORT = 3000;
+
+// Middlewares
+app.use(express.json());
+
+// InitialValues
+const people = [
+  { id: "#1", name: "Person 1", tickets: [] },
+  { id: "#2", name: "Person 2", tickets: [] },
+  { id: "#3", name: "Person 3", tickets: [] },
+  { id: "#4", name: "Person 4", tickets: [] },
+  { id: "#5", name: "Person 5", tickets: [] },
+];
+let ticketCounter = 1;
+let currentAssigneeIndex = 0;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server Running Properly => [POST on /tickets]");
+});
+
+// Create a new ticket
+app.post("/tickets", (req, res) => {
+  try {
+    const { user_id, issue } = req.body;
+    if (!user_id || !issue) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required", success: false });
+    }
+
+    // Assigning Person acccording to the Round Robin Algorithm
+    const assignedPerson = people[currentAssigneeIndex];
+    currentAssigneeIndex = (currentAssigneeIndex + 1) % people.length;
+
+    // Generating new unique id
+    ticketCounter++;
+
+    // Ticket
+    const ticket = {
+      ticket_id: ticketCounter,
+      issue,
+      raisedBy: user_id,
+    };
+    // Response Data
+    const data = {
+      ticket_id: ticketCounter,
+      assigned_to: assignedPerson.id,
+    };
+
+    if (assignedPerson) {
+      assignedPerson.tickets.push(ticket);
+      console.log(assignedPerson);
+      return res
+        .status(201)
+        .json({ message: "Ticket created successfully", success: true, data });
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Assigned person not found", success: false });
+    }
+  } catch {
+    res.status(500).json({ message: "Something went wrong", success: false });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
